4a22e67ac2144a6963d8d29af83b9bf2b0a49e95
CMS Team <cmsdeveloper@unlocklive.com>
2018-02-07 18:26:07 +0600

    login commit

---
+++ includes/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: SHAFIQUL
+ * Date: 2/7/2018
+ * Time: 12:00 PM
+ */
+class Database
+{
+
+}
---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SHAFIQUL
+ * Date: 2/7/2018
+ * Time: 11:56 AM
+ */
